0e95f53eb6046d0c2f5c5ddaacee4ebcd8666316
Vishesh Kumar <vishesh@visheshk.net>
2021-08-15 21:07:04 -0400

    managed the thing! of getting requests

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors')
+var bodyParser = require('body-parser')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -13,13 +15,14 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
-
 app.use(logger('dev'));
 // app.use(bodyParser.urlencoded({ extended: false }))
 // app.use(bodyParser.json())
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({ extended: false , limit: '50mb'}));
+app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
--- routes/data.js
+++ routes/data.js
@@ -8,11 +8,16 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/', function(req, res, next) {
-  res.send(req.body);
-  console.log(req.body);
-  console.log(req.body.answer);
+  // res.send(req.body);
+  // console.log(req.body);
+  // console.log(req.body.test);
+  // console.log(req.test);
+  // console.log(req);
   var io = req.app.get('socketio');
   io.emit('new data', req.body);
+  // res.send(JSON.stringify(req.body));
+  res.send(true);
+  return true;
 });
 
 
